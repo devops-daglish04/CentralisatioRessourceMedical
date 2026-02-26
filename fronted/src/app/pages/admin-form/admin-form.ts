417507c945252956327d9c208d9576c98d039ebc
CEDRIC12KABORE <139150640+CEDRIC12KABORE@users.noreply.github.com>
2026-02-26 19:31:59 +0100

    Initial commit - Angular frontend setup
    j'ai créer le dossier frontend où l'on travaillera

---
+++ fronted/src/app/pages/admin-form/admin-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-form',
+  imports: [],
+  templateUrl: './admin-form.html',
+  styleUrl: './admin-form.css',
+})
+export class AdminForm {}
